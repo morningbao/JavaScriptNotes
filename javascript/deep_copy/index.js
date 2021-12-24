7b24424d2cd15cf3b75184ecc853733d11a12e91
tangchenwei <tangchenwei@ijunhai.com>
2021-12-24 11:38:01 +0800

    clone

---
+++ javascript/deep_copy/index.js
@@ -0,0 +1,11 @@
+const typeStr = {
+    String: '[object String]',
+    Number: '[object Number]',
+    Boolean: '[object Boolean]',
+    string: '[object String]',
+    string: '[object String]',
+    string: '[object String]',
+    string: '[object String]',
+    string: '[object String]',
+}
+function clone() {}
